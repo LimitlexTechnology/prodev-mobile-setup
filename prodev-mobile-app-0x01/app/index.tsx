caa32f02e048d6894604a44744e66f62d3f6ae1b
Limitlex Technologies <kobbygazi@gmail.com>
2026-01-07 19:05:08 +0000

    Implemented UI in index file

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,40 +1,92 @@
-import { Text, View, StyleSheet } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { ImageBackground, Dimensions, View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Entry Screen - Awesome</Text>
-      <View>
-        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-        <Text style={styles.smallText}>ALX is awesome</Text>
-      </View>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        <ImageBackground
+          source={require("@/assets/images/background-image.png")}
+          style={styles.background}
+          resizeMode="cover"
+        >
+          <View style={styles.container}>
+            <View style={styles.companyLogo}>
+              <Image source={require("@/assets/images/Logo.png")} />
+            </View>
+
+            <View style={styles.textGroup}>
+              <Text style={styles.textLarge}>Find your favorite place here</Text>
+              <Text style={styles.textSmall}>The best prices for over 2 </Text>
+              <Text style={styles.textSmall}>million properties worldwide</Text>
+            </View>
+
+            <View style={styles.buttonGroup}>
+              <TouchableOpacity style={styles.button}>
+                <Text style={{ ...styles.textSmall, color: "black" }}>Join here</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.transparentButton}>
+                <Text style={styles.textSmall}>Sign In</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={{ alignItems: "center", paddingVertical: 20 }}>
+              <Text style={{ color: "white" }}>Continue to home</Text>
+            </View>
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+    justifyContent: "center",
+  },
   container: {
-    backgroundColor: "#90caf9",
+    flex: 1,
+    padding: 20,
+    justifyContent: "space-between",
+    backgroundColor: 'rgba(0,0,0,0.3)', // Optional: Adds a dark overlay for better text visibility
   },
-  largeText: {
-    fontSize: 30,
-    color: "#f44336",
-    marginBottom: 5,
-    fontWeight: "700",
-    fontVariant: ["small-caps"],
+  companyLogo: {
+    alignItems: "center",
+    marginTop: 50,
   },
-  mediumText: {
-    fontSize: 20,
-    color: "#9c27b0",
-    marginBottom: 10,
-    fontWeight: "500",
-    textAlign: "right",
+  textGroup: {
+    alignItems: "center",
   },
-  smallText: {
-    fontSize: 15,
-    color: "#2196f3",
-    fontWeight: "400",
+  textLarge: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "white",
     textAlign: "center",
+    marginBottom: 10,
+  },
+  textSmall: {
+    fontSize: 16,
+    color: "white",
+    textAlign: "center",
+  },
+  buttonGroup: {
+    gap: 15,
+  },
+  button: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    borderRadius: 30,
+    alignItems: "center",
+  },
+  transparentButton: {
+    backgroundColor: "transparent",
+    borderWidth: 1,
+    borderColor: "white",
+    paddingVertical: 15,
+    borderRadius: 30,
+    alignItems: "center",
   },
 });

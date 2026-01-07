80b2d25d67bfb947abec8f82aa356245dc8e64ab
Limitlex Technologies <kobbygazi@gmail.com>
2026-01-07 18:58:40 +0000

    Updated index tsx

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -15,24 +15,26 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "#90caf9",
   },
   largeText: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 10,
-    textAlign: "center",
+    fontSize: 30,
+    color: "#f44336",
+    marginBottom: 5,
+    fontWeight: "700",
+    fontVariant: ["small-caps"],
   },
   mediumText: {
-    fontSize: 16,
-    marginBottom: 5,
-    textAlign: "center",
+    fontSize: 20,
+    color: "#9c27b0",
+    marginBottom: 10,
+    fontWeight: "500",
+    textAlign: "right",
   },
   smallText: {
-    fontSize: 12,
-    color: "gray",
+    fontSize: 15,
+    color: "#2196f3",
+    fontWeight: "400",
     textAlign: "center",
   },
 });

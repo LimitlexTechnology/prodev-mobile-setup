c0b3673207a5cc354ac727e895b0c5a8f1716b14
Limitlex Technologies <kobbygazi@gmail.com>
2026-01-23 07:02:11 +0000

    added the required text "Entry Screen - Awesome" and defined the missing styles (container, largeText, mediumText, and smallText)

--- prodev-mobile-app-0x00/app/index.tsx
+++ prodev-mobile-app-0x00/app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.largeText}>Entry Screen - Awesome</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  largeText: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  mediumText: {
+    fontSize: 20,
+  },
+  smallText: {
+    fontSize: 10,
+  },
+});
